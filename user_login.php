534b8dbb4886758b0befb943497cd45e1e574651
Corey <cor.johnson@me.com>
2014-12-15 10:01:12 -0800

    link back to login on incorrect user/pass

--- user_login.php
+++ user_login.php
@@ -1,47 +1,48 @@
 <?php
-// Start Session
-session_start();
+	// Start Session
+	session_start();
 
-// Start the buffer
-ob_start();
+	// Start the buffer
+	ob_start();
 
-ini_set('display_errors',1); 
- error_reporting(E_ALL);
+	ini_set('display_errors',1); 
+	error_reporting(E_ALL);
 
-//Database
-require "db.php";
+	//Database
+	require "db.php";
 
-try {
-	$dbh = new PDO("mysql:host=$hostname;
-					dbname=caseym_Aviation", $username, $password);
-} catch (PDOException $e){
-	echo $e->getMessage();
-}
- $tbl_name = "UserInfo";
+	try {
+		$dbh = new PDO("mysql:host=$hostname;
+						dbname=caseym_Aviation", $username, $password);
+	} catch (PDOException $e) {
+		echo $e->getMessage();
+	}
+	$tbl_name = "UserInfo";
 
-// Define $myusername and $mypassword from User Form
-$myusername=$_POST['myusername'];
-$mypassword=$_POST['mypassword'];
+	// Define $myusername and $mypassword from User Form
+	$myusername=$_POST['myusername'];
+	$mypassword=$_POST['mypassword'];
 
 
-$sql="SELECT * FROM $tbl_name WHERE email='$myusername' and password='$mypassword'";
-$result=$dbh->query($sql);
+	$sql="SELECT * FROM $tbl_name WHERE email='$myusername' and password='$mypassword'";
+	$result=$dbh->query($sql);
 
-// Mysql_num_row is counting table row
-$count=$result->rowCount();
+	// Mysql_num_row is counting table row
+	$count=$result->rowCount();
 
-// If result matched $myusername and $mypassword, table row must be 1 row
+	// If result matched $myusername and $mypassword, table row must be 1 row
 
-if($count==1){
+	if($count==1){
 
-// Register $myusername, $mypassword and redirect to file "user_login_success.php"
-$_SESSION["myusername"] = $myusername;
-$_SESSION["mypassword"]= $mypassword;
-header("location:main.php");
-}
-else {
-echo "Wrong Username or Password";
-}
+		// Register $myusername, $mypassword and redirect to file "user_login_success.php"
+		$_SESSION["myusername"] = $myusername;
+		$_SESSION["mypassword"]= $mypassword;
+		header("location:main.php");
+	}
+	else {
+		echo "<h3>Wrong Username or Password<h3>
+			<a href='index.html'>Back to login</a>";
+	}
 
-ob_end_flush();
+	ob_end_flush();
 ?>

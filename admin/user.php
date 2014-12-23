c6f608008341b27f9441b97551896e14d3b5cbfd
Casey Morris <casemorris@hotmail.com>
2014-12-23 13:48:24 -0800

    Removed older users.php and updated user.php
    Removed generic email

--- admin/user.php
+++ admin/user.php
@@ -21,35 +21,9 @@
         // Isset used for buttons
         //echo "Form was submitted.";
         
-        //Flag variable for valid data. Depending on what fails determines what saves
-        $validEmail = true;
+        //Flag variable for valid data.
         $validPassword = true;
     
-    
-        
-        if (!empty($_POST['email'])){
-            
-            $email = $_POST['email'];
-            // Sanitize email
-            $emailSanitized = filter_var($email, FILTER_SANITIZE_EMAIL);
-            if (filter_var($emailSanitized, FILTER_VALIDATE_EMAIL))
-            {
-                if(strpos($emailSanitized, "@greenriver.edu")=== false && strpos($emailSanitized, "@mail.greenriver.edu")=== false){
-                    $emailErr="<p class='error'>Email address is not the correct Green River email.</p>";
-                    echo $emailSanitized;
-                    $validEmail = false;
-                }
-            }
-            else
-            {
-                $emailErr = "<p class='error'>Email address invalid!</p>";
-                $validEmail = false;
-            }
-           
-        }else{
-            $validEmail = false;
-            }  
-        
         //Password
         if (!empty($_POST['password'])) {
             $password= $_POST['password'];
@@ -63,16 +37,7 @@
         
 
         //Depending on what is valid, determines what is updated
-        if( $validEmail && $validPassword){
-            //Both email and password update
-            $sql = "UPDATE UserInfo SET email = :email, password = :password where id = '1'";
-            $statement = $dbh->prepare($sql);
-            $statement->bindParam(':email',$_POST['email']);
-            $statement->bindParam(':password',$_POST['password']);
-            $statement->execute();
-            echo"Email and password has changed!";
-        }
-        else if (!$validEmail && $validPassword){
+        if ($validPassword){
             //Password updates only WHERE sectitle = :key
             $sql = "UPDATE UserInfo SET password = :password where id = '1'";
             $statement = $dbh->prepare($sql);
@@ -80,21 +45,14 @@
             $statement->execute();
             echo"Password has changed!";
         }
-        else if ($validEmail && !$validPassword){
-            //Email updates only
-            $sql = "UPDATE UserInfo SET email = :email where id = '1'";
-            $statement = $dbh->prepare($sql);
-            $statement->bindParam(':email',$_POST['email']);
-            $statement->execute();
-            echo"Email address has changed!";
-        }
+       
         
         
     }
     
 ?>
 <h2>User Account</h2>
-<p>This is our universal login that anyone in the aviation class can use.</p>
+<p>Administrator can change the student's password.</p>
 <?php
 
     //Display contacts from database
@@ -102,19 +60,12 @@
     $result = $dbh->query($sql);
     foreach ($result as $row)
     {
-        $userEmail = $row['email'];
         $userPass = $row['password'];
     }
 
 ?>
  <div class="row">
         <div class="panel panel-info col-md-5" >
-                <div class="panel-heading">
-                    <h3 class="panel-title">Current Email:</h3>
-                </div>
-                <div class="panel-body">
-                    <?php printf("<p>%s</p>", $userEmail); ?>
-                </div>
                 <div class="panel-heading">
                     <h3 class="panel-title">Current Password:</h3>
                 </div>
@@ -125,11 +76,6 @@
         </div>
         <div class="col-md-5">
             <form role="form" action="adminMenu.php?page=user" method="post">
-              
-              <div class="form-group">
-                <label for="email">Email Address:</label><?php echo "<span class='alert-danger' >".$emailErr."</span>"; ?>
-                <input type="email" class="form-control" name="email" id="email">
-              </div>
               <div class="form-group">
                 <label for="password">Password</label><?php echo "<span class='alert-danger' >".$passErr."</span>"; ?>
                 <input type="password" class="form-control" name="password" id="password">
